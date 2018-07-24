0888de8fcc2bb6aa2c8e7430740d2c9b764ac0d9
Madeline Celis <madelinebcelis@gmail.com>
2018-07-24 01:49:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,14 @@ def key_for_min_value(name_hash)
    min = nil
 
    name_hash.each do |key, value|
-
+     if min === nil
+       min = value
+     else
+       if value < min
+         min = value
+         minKey = key
+       end
+     end
    end
 
    minKey
